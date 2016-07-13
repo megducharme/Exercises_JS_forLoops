ce7cab8200c4bb7ca69fbff104f5c44f542704ec
Meg Ducharme <ducharme.meg@gmail.com>
2016-07-13 13:08:54 -0500

    First Commit

---
+++ for-loops.js
@@ -0,0 +1,16 @@
+
+// for (var i = 5; i < 120; i += 10) {
+//   console.log (i);
+// }
+ 
+
+// for (var i = 4096; i >= 1; i /= 2) {
+//   console.log (i);
+// }
+ 
+ // console.log("Meg")
+
+ var presidents = ["George Washington", "John Adams", "Thomas Jefferson", "Bill Clinton", "George Bush"]
+ for (var i = 0; i < presidents.length; i ++) {
+    console.log("President #" + (i + 1) + " " + presidents[i]);
+ }
